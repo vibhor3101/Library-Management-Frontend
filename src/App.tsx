39d4d016ce5521e06d44cdf41c870080dbce3142
Vibhor <vibhorsingla3104@gmail.com>
2024-06-02 15:38:48 +0530

    Frontend initialization

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+};
+
+//jsx
+//package.json
+//package-lock,json
+//node-modules
+//public folder in react
